612653cf3cd045a6ff3d1086dac467662a7599f4
medmaha <toure925@outlook.com>
2026-08-22 17:18:59 +0000

    add MIT License file

--- lib/feedback.ts
+++ lib/feedback.ts
@@ -5,19 +5,15 @@ import DB from "./db";
 import { feedbacks } from "./db/schema";
 
 export async function createFeedback(data: typeof feedbacks.$inferInsert) {
-  const req = await request();
-  const decision = await arcjetMiddlewareFeedback.protect(req);
-  if (decision.isDenied()) {
-    if (decision.isDenied()) {
-      if (decision.reason.isRateLimit()) {
-        throw new Error("You've hit the rate limit please try again in 2mins");
-      } else if (decision.reason.isBot()) {
-        throw new Error("No bots allowed");
-      } else {
-        throw new Error("Forbidden");
-      }
-    }
-  }
-  await DB.insert(feedbacks).values(data);
-  return true;
+	const req = await request();
+	const decision = await arcjetMiddlewareFeedback.protect(req);
+	if (decision.isDenied()) {
+		if (decision.reason.isRateLimit()) {
+			throw new Error("You've hit the rate limit please try again in 2mins");
+		} else {
+			throw new Error("Forbidden");
+		}
+	}
+	await DB.insert(feedbacks).values(data);
+	return true;
 }
